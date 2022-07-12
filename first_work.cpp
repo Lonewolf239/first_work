cb3f8b3e7d57f71b7860a6674a4029d80f9732b7
Lonewolf239 <semga05@mail.ru>
2022-07-12 08:09:41 +0300

    Add first_work.cpp

---
+++ first_work.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+int main() {
+	printf("Hello,Friend)\n");
+	short x = 4, y = 2, res;
+	x++;
+	y *= 2;
+	res = x + y;
+	printf("%d\n", res);
+	float a = 2.62343f, b = 5.23432f, tes;
+	a *= 2;
+	b--;
+	b += 3.3432f;
+	tes = a / b;
+	printf("%.2f\n", tes);
+	char q = '1', w = 'd', e = 'G', r = '%';
+	q = '5';
+	printf("%c,%c,%c,%c\n",q,w,e,r);
+}
